5156c1de929c6b282c925cdbd4fdfa4dc7dafac7
Adoniran <adoniranfranca@gmail.com>
2025-06-03 18:02:24 -0300

    chore: configura Vite e dependências iniciais

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      'vue': 'vue/dist/vue.esm-bundler.js'
+    }
+  },
+})
