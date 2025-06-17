2e95ecce118e76e04a1bdc99c3b004718bb917ad
Adoniran <adoniranfranca@gmail.com>
2025-06-16 21:25:20 -0300

    feat(ui): cria componente Location para exibição de endereço

---
+++ src/components/ui/Location.vue
@@ -0,0 +1,13 @@
+<template>
+  <span>
+    <Icon icon="system-uicons:location" style="width: 20px; height: 20px;" />
+    {{ location }}
+  </span>
+</template>
+
+<script setup>
+  import { Icon } from '@iconify/vue';
+  defineProps({
+    location: String
+  })
+</script>
