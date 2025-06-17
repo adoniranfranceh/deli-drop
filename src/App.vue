b8e8484ed869772f768ebfb92f0a9384defbda45
Adoniran <adoniranfranca@gmail.com>
2025-06-16 21:23:18 -0300

    feat(layout): adiciona componente CartBar fixo no App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,28 @@
     <NavBar />
     <main id="container">
       <router-view />
+      <CartBar />
     </main>
   </div>
 </template>
 
 <script setup>
 import NavBar from '@/components/Navbar.vue'
+import CartBar from './components/CartBar.vue';
+
+import { watch } from 'vue'
+import { useUIStore } from '@/stores/uiStore'
+
+const ui = useUIStore()
+
+watch(() => ui.isMobileModalOpen, (isOpen) => {
+  const isMobile = window.innerWidth <= 768
+  if (isMobile && isOpen) {
+    document.body.classList.add('modal-open')
+  } else {
+    document.body.classList.remove('modal-open')
+  }
+})
 </script>
 
 <style scoped>
