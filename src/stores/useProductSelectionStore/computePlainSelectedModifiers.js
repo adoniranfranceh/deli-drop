b7c32b3e3aa19bf51d15bbcfa32d905e99da0832
Adoniran <adoniranfranca@gmail.com>
2025-07-06 14:06:17 -0300

    refactor(useProductSelectionStore): remove legacy and duplicated helper files

--- src/stores/useProductSelectionStore/computePlainSelectedModifiers.js
+++
@@ -1,8 +0,0 @@
-export function computePlainSelectedModifiers(selectedModifiers) {
-  return selectedModifiers.value.map(mod => ({
-    id: mod.id,
-    min: mod.min,
-    max: mod.max,
-    selected: mod.selected
-  }))
-}
