113eff47ff10d70639d10f1a4a29460f4276a4ae
Adoniran <adoniranfranca@gmail.com>
2025-10-01 10:39:43 -0300

    chore(api): adicionar headers padrão na instância axios

--- src/stores/productSelectionStore/utils/restaurantUtils.js
+++ src/stores/productSelectionStore/utils/restaurantUtils.js
@@ -3,7 +3,10 @@ import axios from 'axios'
 let data_restaurants = []
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    Accept: 'application/json'
+  }
 })
 
 export async function fetchRestaurant(id) {
