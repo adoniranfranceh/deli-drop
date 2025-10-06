528fb568d29a663e55c703c7479de048c69d3666
Adoniran <adoniranfranca@gmail.com>
2025-10-06 19:14:12 -0300

    feat(featured-products): adiciona rolagem infinita ao componente FeaturedProducts.vue

--- src/components/ui/FeaturedProducts.vue
+++ src/components/ui/FeaturedProducts.vue
@@ -1,41 +1,37 @@
 <template>
-  <div class="products-container">
-    <CardProduct
-      v-for="product in products"
-      :key="product.id"
-      :product="product"
-      :showCategory="showCategory"
-      @selected-product="openModal"
-      @open-modal="openModal"
-    />
+  <div>
+    <InfiniteScroll
+      :items="products"
+      :perPage="10"
+      v-slot="{ items }"
+      class="products-container"
+    >
+      <CardProduct
+        v-for="product in items"
+        :key="product.id"
+        :product="product"
+        :showCategory="showCategory"
+        @selected-product="openModal"
+      />
 
-    <ProductModalManager
-      ref="modalManager"
-      :products="products"
-    />
+      <ProductModalManager ref="modalManager" :products="items" />
+    </InfiniteScroll>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-
 import CardProduct from '@/components/product/CardProduct.vue'
 import ProductModalManager from '@/components/product/ProductModalManager.vue'
+import InfiniteScroll from '@/components/ui/InfiniteScroll.vue'
 
 defineProps({
-  products: {
-    type: Array,
-    required: true
-  },
-  showCategory: Boolean,
-  bgColor: String
+  products: { type: Array, required: true },
+  showCategory: Boolean
 })
 
 const modalManager = ref(null)
-
-const openModal = (product) => {
-  modalManager.value?.openModal(product)
-}
+const openModal = (product) => modalManager.value?.openModal(product)
 </script>
 
 <style scoped>
@@ -47,7 +43,7 @@ const openModal = (product) => {
   grid-template-columns: repeat(1, 1fr);
 }
 
- @media (min-width: 758px) {
+@media (min-width: 758px) {
   .products-container {
     grid-template-columns: repeat(2, 1fr);
   }
